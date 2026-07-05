6097636c7e1f4b0d41c17be8b34a13914fe79214
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-05 11:21:59 +0530

    Added round wise cutoff analysis

--- Uptac_Smart_Counselling_Assistant/src/analysis.py
+++ Uptac_Smart_Counselling_Assistant/src/analysis.py
@@ -10,8 +10,27 @@ def load_data():
     return df
 
 
+def round_wise_analysis(df):
+
+    print("\nROUND WISE ANALYSIS")
+    print("-" * 50)
+
+    round_count = df["Round"].value_counts().sort_index()
+
+    print(round_count)
+
+    print("\nTotal Rounds :", df["Round"].nunique())
+
+    print("\nHighest Records")
+    print(round_count.max())
+
+    print("\nLowest Records")
+    print(round_count.min())
+
+
 def main():
     df = load_data()
+    round_wise_analysis(df)
 
 
 if __name__ == "__main__":
