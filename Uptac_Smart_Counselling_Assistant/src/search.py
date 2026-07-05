21240ea623716ba5ba9d9f6de4bbb2daac85077f
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-05 12:31:54 +0530

    Added institute search functionality

--- Uptac_Smart_Counselling_Assistant/src/search.py
+++ Uptac_Smart_Counselling_Assistant/src/search.py
@@ -8,8 +8,23 @@ def load_data():
     return df
 
 
+def clg_search(df):
+
+    clg = input("Enter college name : ").strip().lower()
+    df["Institute"] = df["Institute"].str.strip().str.lower()
+
+    search = df[df["Institute"].str.contains(clg)]
+
+    if search.empty:
+        print("College not found")
+
+    else:
+        print(search)
+
+
 def main():
     df = load_data()
+    clg_search(df)
 
 
 if __name__ == "__main__":
