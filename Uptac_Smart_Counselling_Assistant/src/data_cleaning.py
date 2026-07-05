a254f92b061fddb5b24123383fd2d2b893123a48
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-05 10:57:21 +0530

    Initialize project structure

---
+++ Uptac_Smart_Counselling_Assistant/src/data_cleaning.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+df = pd.read_csv("Uptac_Smart_Counselling_Assistant/data/raw/uptac_cutoff_2025.csv")
+
+
+def load_data():
+    df = pd.read_csv("Uptac_Smart_Counselling_Assistant/data/raw/uptac_cutoff_2025.csv")
+    return df
+
+
+def data_overview(df):
+    print("-" * 60)
+    print("DATASET OVERVIEW")
+    print("-" * 50)
+
+    print("\nFIRST FIVE ROWS")
+    print(df.head(5))
+
+    print(f"\nTotal row              : {df.shape[0]}")
+    print(f"Total Columns          : {df.shape[1]}")
+    print(f"Total Null Values      : {df.isnull().sum().sum()}")
+    print(f"Total Duplicate Values : {df.duplicated().sum()}")
+
+    print("\nMISSING VALUES ")
+    print("-" * 50)
+    print(df.isnull().sum())
+
+    print(" \n DATA TYPES")
+    print("-" * 50)
+    print(df.dtypes)
+
+    print("\nNUMERICAL SUMMARY")
+    print("-" * 50)
+    print(df.describe())
+
+
+def main():
+    df = load_data()
+    data_overview(df)
+
+
+if __name__ == "__main__":
+    main()
